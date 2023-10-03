6dcdfc500235ebcdd5facc4c3a3f09a2f451b471
Lailatul Qomariah <55173357+Lailatul-qomariah@users.noreply.github.com>
2023-10-03 22:00:40 +0700

    Enums

---
+++ API/Utilities/Enums/GenderLevels.cs
@@ -0,0 +1,8 @@
+﻿namespace API.Utilities.Enums
+{
+    public enum GenderLevels //menentukan gender level di modelnya 
+    {
+            Female, //default numeriknya dimulai dari 0. 
+            Male //numeriknya adalah 1 
+    }
+}
---
+++ API/Utilities/Enums/StatusLevels.cs
@@ -0,0 +1,13 @@
+﻿namespace API.Utilities.Enums
+{
+    public enum StatusLevels //menentukan gender level di modelnya
+    {
+        Requested, //default numeriknya dimulai dari 0 sama seperti index
+        Approved, //1
+        Rejected, //2
+        Canceled, //3
+        Completed, //4
+        OnGoing //5
+
+    }
+}
